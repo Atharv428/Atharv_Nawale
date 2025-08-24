ccd3aee3d1b1baf3603629fb0f850e6bc22e55c4
Atharv428 <atharvnawale100@gmail.com>
2025-08-24 12:23:50 +0530

    Create Day9.cpp

---
+++ Day9.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> mp;
+
+    for (string s : strs) {
+        string key = s;
+        sort(key.begin(), key.end());  // sorted string as key
+        mp[key].push_back(s);
+    }
+
+    vector<vector<string>> result;
+    for (auto& it : mp) {
+        result.push_back(it.second);
+    }
+    return result;
+}
+
+int main() {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    vector<vector<string>> groups = groupAnagrams(strs);
+
+    // Print result
+    for (auto& group : groups) {
+        cout << "[ ";
+        for (auto& word : group) {
+            cout << word << " ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
